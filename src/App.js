35fd64b603f654f9651d9e10392aed649f75ee9e
smfssilva <smfssilva@yahoo.com>
2022-03-05 15:59:29 -0300

    27 - component state

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello my name is Silva
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      name: 'Sivanilson'
+    }
+  }
+
+  onClickHandler = () => {
+    if (this.state.name === 'Silva') {
+      this.setState({ name: 'Sivanilson' })
+    } else {
+      this.setState({ name: 'Silva' })
+    }
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Hi {this.state.name}
+          </p>
+          <button
+            onClick={this.onClickHandler}
+          >Change Name</button>
+        </header>
+      </div >
+    )
+  }
 }
 
 export default App;
